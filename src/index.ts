080d7b88bb2ceff6106d5753e9540be9223f8867
mdornseif <md@foxel.org>
2026-01-13 12:40:03 +0100

    Delete src directory

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * index.ts
- *
- * Created by Dr. Maximillian Dornseif 2023-04-24 in datastore-simulator 0.1.0
- * Copyright (c) 2023 Maximillian Dornseif
- */
-
-export { Datastore } from './lib/datastore-simulator'
